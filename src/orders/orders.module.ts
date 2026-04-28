daf09c9f8b55d70da41915db283d9072f2f448b1
shubhda-katkar <katkarshubhada0@gmail.com>
2026-04-28 18:00:38 +0530

    Added Orders

--- src/orders/orders.module.ts
+++ src/orders/orders.module.ts
@@ -1,16 +1,20 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from '../users/schemas/user.schema';
+import { Notification, NotificationSchema } from '../users/schemas/notification.schema';
 import { OrdersKafkaController } from './orders.kafka.controller';
 import { OrdersController } from './orders.controller';
 import { OrdersService } from './orders.service';
 import { Order, OrderSchema } from './schemas/order.schema';
+import { UsersModule } from '../users/users.module';
 
 @Module({
   imports: [
+    forwardRef(() => UsersModule),
     MongooseModule.forFeature([
       { name: Order.name, schema: OrderSchema },
       { name: User.name, schema: UserSchema },
+      { name: Notification.name, schema: NotificationSchema },
     ]),
   ],
   controllers: [OrdersController, OrdersKafkaController],
